a19a5500b2a78d7dae9f8adb1ebba05120f8c261
netnr <netnr@netnr.com>
2019-03-19 16:45:25 +0800

    update

--- src/Netnr.WeChat/Netnr.WeChat/Helpers/HttpTo.cs
+++ src/Netnr.WeChat/Netnr.WeChat/Helpers/HttpTo.cs
@@ -4,18 +4,20 @@ using System.Text;
 
 namespace Netnr.WeChat
 {
+    /// <summary>
+    /// HTTP请求
+    /// </summary>
     public class HttpTo
     {
-        #region 服务器 发送请求
-
         /// <summary>
-        /// 发送请求参数设置
+        /// HttpWebRequest对象
         /// </summary>
         /// <param name="url">地址</param>
-        /// <param name="type">请求类型 默认GET </param>
-        /// <param name="postData">POST发送内容 默认空</param>
+        /// <param name="type">请求类型，默认GET</param>
+        /// <param name="data">POST数据</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
-        public static HttpWebRequest HWRequest(string url, string type = "GET", string postData = "")
+        public static HttpWebRequest HWRequest(string url, string type = "GET", string data = null, string charset = "utf-8")
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = type;
@@ -25,11 +27,11 @@ namespace Netnr.WeChat
             request.Timeout = short.MaxValue * 3;//MS
             request.ContentType = "application/x-www-form-urlencoded";
 
-            if (type != "GET")
+            if (type != "GET" && data != null)
             {
                 //发送内容
-                byte[] bytes = Encoding.UTF8.GetBytes(postData);
-                request.ContentLength = Encoding.UTF8.GetBytes(postData).Length;
+                byte[] bytes = Encoding.GetEncoding(charset).GetBytes(data);
+                request.ContentLength = Encoding.GetEncoding(charset).GetBytes(data).Length;
                 Stream outputStream = request.GetRequestStream();
                 outputStream.Write(bytes, 0, bytes.Length);
                 outputStream.Close();
@@ -38,60 +40,49 @@ namespace Netnr.WeChat
         }
 
         /// <summary>
-        /// 发送请求 得到请求结果
+        /// HTTP请求
         /// </summary>
-        /// <param name="request">HttpWebRequest对象 可通过HWRequest方法创建</param>
-        /// <param name="e">返回类容编码 默认UTF-8</param>
+        /// <param name="request">HttpWebRequest对象</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
-        public static string Url(HttpWebRequest request, Encoding e)
+        public static string Url(HttpWebRequest request, string charset = "utf-8")
         {
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
             Stream responseStream = response.GetResponseStream();
             if (string.Compare(response.ContentEncoding, "gzip", true) >= 0)
                 responseStream = new System.IO.Compression.GZipStream(responseStream, System.IO.Compression.CompressionMode.Decompress);
-            StreamReader reader = new StreamReader(responseStream, e);
-            string result = "", strData = "";
-            //result = reader.ReadToEnd();
-            while ((strData = reader.ReadLine()) != null)
+
+            using (var sr = new StreamReader(responseStream, Encoding.GetEncoding(charset)))
             {
-                result += strData + "\r\n";
+                var result = sr.ReadToEnd();
+                return result;
             }
-            reader.Close();
-            responseStream.Close();
-            return result;
         }
 
         /// <summary>
-        /// 发送请求 得到请求结果
+        /// GET请求
         /// </summary>
-        /// <param name="request">HttpWebRequest对象 可通过HWRequest方法创建</param>
+        /// <param name="url">地址</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
-        public static string Url(HttpWebRequest request)
+        public static string Get(string url, string charset = "utf-8")
         {
-            return Url(request, Encoding.UTF8);
+            var request = HWRequest(url, "GET", null, charset);
+            return Url(request, charset);
         }
 
         /// <summary>
-        /// 发送 GET 请求 得到请求结果
+        /// POST请求
         /// </summary>
-        /// <param name="url">URL</param>
+        /// <param name="url">地址</param>
+        /// <param name="data">POST数据</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
-        public static string Get(string url)
+        public static string Post(string url, string data, string charset = "utf-8")
         {
-            return Url(HWRequest(url));
+            var request = HWRequest(url, "POST", data, charset);
+            return Url(request, charset);
         }
-
-        /// <summary>
-        /// 发送 POST 请求
-        /// </summary>
-        /// <param name="url">URL</param>
-        /// <param name="postData">发送内容</param>
-        /// <returns></returns>
-        public static string Post(string url, string postData)
-        {
-            return Url(HWRequest(url, "POST", postData.ToEncode()));
-        }
-
-        #endregion
     }
 }
